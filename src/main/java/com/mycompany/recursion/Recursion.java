bbf264338a79b9ba1060dbf9fa883b0d6145294d
Erika Bautista <ekabells19@gmail.com>
2024-01-15 01:49:54 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/recursion/Recursion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursion;
+
+/**
+ *
+ * @author Erika
+ */
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
